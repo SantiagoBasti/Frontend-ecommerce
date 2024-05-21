eb73da19195354cc1f63088f584915e95f8f2372
Santiago <santiago.bastidascaro@gmail.com>
2024-05-20 20:07:28 -0500

    Rutas hijas con layout y login separado

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import './App.css'
-import Header from './layout/header/Header';
-import Footer from './layout/footer/Footer';
 import Home from './pages/home/Home';
 import Login from './pages/login/Login';
 import Contact from './pages/contact/Contact';
@@ -12,26 +10,29 @@ import NotFound from './pages/not-found/NotFound';
 import ProductDetail from './pages/product-detail/ProductDetail';
 import { Route, Routes } from 'react-router-dom';
 import AdminGuard from './services/guard/AdminGuard';
+import Layout from './layout/Layout';
 
 
 function App() {
   return (
     <>
-      <Header />
 
-      <main className="main-container">
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/about-us" element={<AboutUs />} />
+
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Layout />} >
+
+        <Route index element={<Home />} />
+
+          <Route path="contact" element={<Contact />} />
+          <Route path="register" element={<Register />} />
+          <Route path="about-us" element={<AboutUs />} />
           {/* Ruta con params */}
-          <Route path="/product-detail/:id" element={<ProductDetail />} />
+          <Route path="product-detail/:id" element={<ProductDetail />} />
 
           {/* Rutas protegidas */}
           <Route
-            path="/admin-product"
+            path="admin-product"
             element={
               <AdminGuard>
                 {/* Componente hijo: children */}
@@ -40,19 +41,19 @@ function App() {
             }
           />
           <Route
-            path="/admin-user"
+            path="admin-user"
             element={
               <AdminGuard>
                 <AdminUser />
               </AdminGuard>
             }
           />
-
           <Route path="*" element={<NotFound />} />
-        </Routes>
-      </main>
 
-      <Footer />
+        </Route>
+
+        </Routes>
+        
     </>
   );
 }
